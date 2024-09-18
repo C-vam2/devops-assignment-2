fe0935986546502c64ed688c5d62e3fd13709935
vishwateja21 <vishwapeddakapu21@gmail.com>
2024-09-19 00:03:49 +0530

    insertionsort

--- SortFunctions.cpp
+++ SortFunctions.cpp
@@ -23,6 +23,17 @@ void mergeSort(vector<int>& arr) {
 void quickSort(vector<int>& arr) {
 
 }
+void insertionSort(vector<int>& arr){
+    for(int i = 1;i < arr.size() ; i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key
+    }
+}
 
 void sortArray(vector<int>& arr, SortAlgorithm algorithm) {
     switch (algorithm) {
